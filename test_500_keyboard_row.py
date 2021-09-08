dce0a08fe7f1694e3a29f786d359f191c206ab79
Ryan Xie <ryan.xie@doordash.com>
2021-09-08 08:59:01 -0700

    [LC 0500] add - hashmap

---
+++ test_500_keyboard_row.py
@@ -0,0 +1,37 @@
+import unittest
+
+"""
+write down thoughts
+
+
+"""
+from typing import List
+
+
+class Solution:
+    def findWords(self, words: List[str]) -> List[str]:
+        key_map = {}
+        for key in "qwertyuiopQWERTYUIOP":
+            key_map[key] = 0
+        for key in "asdfghjklASDFGHJKL":
+            key_map[key] = 1
+        for key in "zxcvbnmZXCVBNM":
+            key_map[key] = 2
+        res = []
+        for word in words:
+            row = key_map[word[0]]
+            for letter in word:
+                if key_map[letter] != row:
+                    break
+            else:
+                res.append(word)
+        return res
+
+
+class TestSolution(unittest.TestCase):
+    def test1(self):
+        self.assertEqual(["Alaska", "Dad"], Solution().findWords(["Hello", "Alaska", "Dad", "Peace"]))
+
+
+if __name__ == '__main__':
+    unittest.main()
